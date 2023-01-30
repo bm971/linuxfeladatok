9ec150d7849ade94776c2a1657882475a7e99dbb
bm971 <bakonyimark9785@gmail.com>
2023-01-30 10:47:20 +0100

    helpscript1.2

--- feladatok_linux/linux_2/runtest.sh
+++ feladatok_linux/linux_2/runtest.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # run at ~/playground/linuxfeladatok_pg/
+# $1 script-to-be-run
 
 cp $1 dumpdir/$1 # copy the given file to the dumpdir directory
 chmod 774 dumpdir/$1 # make executable for user and group, readonly for others
