dfd88735c2fa3abf7e86a400c3215e32b569b061
bakonyim <49881758+bm971@users.noreply.github.com>
2023-02-02 08:28:24 +0100

    update 3feladat

--- feladatok_linux/linux_1/3feladat.sh
+++ feladatok_linux/linux_1/3feladat.sh
@@ -9,13 +9,35 @@
 
 groupadd -g 80 losers
 groupadd -g 100 users
+groupadd -g 120 students
+useradd -s /bin/bash -m -u 1200 -g students student
 useradd -s /bin/bash -m -d /home/abc123 -u 2000 -g users user
 useradd -s /bin/bash -m -u 3000 -g losers loser 
 
+#if umask == 0022
+
 install -m 750 -d -o user -g users www
+#user::rwx
+#group::r-x
+setfacl -m m:r-x www
+#other::---
+setfacl -d -m "u::rwx" www
+#default:group::r-x
+setfacl -d -m "g:losers:rwx" www
+setfacl -d -m m:rwx www
+setfacl -d -m o:r-x www
+
 install -m 751 -d -o user -g users www/htdocs
+#user::rwx
+#group::r-x
+setfacl -m "g:students:r--" www/htdocs/
+setfacl -m m:r-x www/htdocs
+#other::--x
+
 chmod 644 www/htdocs/index.php>>www/htdocs/index.php
-
-#chown loser:students www/htdocs/index.php
-
-#setfacl -m "g:losers:rwx" www
+chown user:students www/htdocs/index.php
+#user::rw-
+setfacl -m "u:loser:rwx" www/htdocs/index.php
+setfacl -m "g::r--" www/htdocs/index.php
+#mask::rwx
+#other::r--
